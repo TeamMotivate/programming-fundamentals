a12d97b4ad66fae3fe241b4b77ba32b0e8747c51
konstantinnelkov88 <nelkov88@gmail.com>
2018-03-10 17:52:55 +0200

    update

--- intro-and-basic-syntax/exercise/MilesToKilometers.cs
+++ intro-and-basic-syntax/exercise/MilesToKilometers.cs
@@ -8,6 +8,7 @@ namespace p03_MilesToKilometers
         {
 			//comment from nelkov
 			//comment
+			//comment 2
             double miles = double.Parse(Console.ReadLine());
             double kilometers = miles * 1.60934;
 
