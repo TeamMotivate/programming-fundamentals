240c80132182a381e2b2751e367af5a2b2563dc9
Mitko1239 <dimitarcho1@gmail.com>
2018-03-10 18:10:45 +0200

    Renamed variables in RectangleArea.cs

--- intro-and-basic-syntax/exercise/RectangleArea.cs
+++ intro-and-basic-syntax/exercise/RectangleArea.cs
@@ -7,14 +7,14 @@ namespace _02._Rectangle_Area
         static void Main(string[] args)
         {
 			// Receiving the two sides of the rectangle and parsing them to double
-            double a = double.Parse(Console.ReadLine());
-            double b = double.Parse(Console.ReadLine());
+            double sideA = double.Parse(Console.ReadLine());
+            double sideB = double.Parse(Console.ReadLine());
 			
 			// Calculating the area
-            double ab = a * b;
+            double area = sideA * sideB;
 			
 			// Printing the area to the console
-            Console.WriteLine($"{ab:F2}");
+            Console.WriteLine($"{area:F2}");
         }
     }
 }
