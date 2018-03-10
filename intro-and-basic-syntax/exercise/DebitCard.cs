7180cc277fdfa1234f8d7c7b5d80c2b1c937abed
IliyanTodorov <mikeband@abv.bg>
2018-03-10 18:28:24 +0200

    Everything is OK

--- intro-and-basic-syntax/exercise/DebitCard.cs
+++ intro-and-basic-syntax/exercise/DebitCard.cs
@@ -16,7 +16,7 @@ namespace DebitCardNumber
             var n3 = int.Parse(Console.ReadLine());
             var n4 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("{0:D4} {1:D4} {2:D4} {3:D4}", n1, n2, n3, n4);
+            Console.WriteLine("{0:D4} {1:D4} {2:D4} {3:D4}", n1, num2, n3, n4);
         }
     }
 }
