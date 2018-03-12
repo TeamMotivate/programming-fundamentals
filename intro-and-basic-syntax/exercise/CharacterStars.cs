5c4b82cb1afcf2edf15b692fbc849435704a9026
Patronov <gtpatronov@gmail.com>
2018-03-12 23:32:07 +0200

    Added CharacterStars Solution

---
+++ intro-and-basic-syntax/exercise/CharacterStars.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Character_Stats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int currentHealth = int.Parse(Console.ReadLine());
+            int maximumHealth = int.Parse(Console.ReadLine());
+            int currentEnergy = int.Parse(Console.ReadLine());
+            int maximumEnergy = int.Parse(Console.ReadLine());
+            int restHealt = maximumHealth - currentHealth;
+            int restEnergy = maximumEnergy - currentEnergy;
+            string currentHealthAsChar= new String('|', currentHealth);
+            string restHealthAsChar = new String('.', restHealt);
+            string currentEnergyAsChar= new String('|', currentEnergy);
+            string restEnergyAsChar=new String('.', restEnergy);
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Health: |{currentHealthAsChar}{restHealthAsChar}|");
+            Console.WriteLine($"Energy: |{currentEnergyAsChar}{restEnergyAsChar}|");
+        }
+    }
+}
